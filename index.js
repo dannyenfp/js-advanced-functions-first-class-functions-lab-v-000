4ddad3a30048ae4e39a698bff347bb500a47760a
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-23 21:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const returnLastTwoDrivers = function(drivers) {
 }
 
 const selectingDrivers = function(drivers){ 
-  return [returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers)];
+  return returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers);
 } 

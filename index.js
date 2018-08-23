a94ee5d79a27c92830d5c1feeea47a2eca87df2b
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-23 21:38:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(drivers.length-2, drivers.length);
 }
 
-const selectingDrivers = [return returnFirstTwoDrivers, returnLastTwoDrivers];
+const selectingDrivers = return [ returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(num) = function(fare) { 
   return function(fare*4); 
